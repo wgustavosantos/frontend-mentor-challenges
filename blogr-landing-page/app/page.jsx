3450a2262a52f8fa4c5872a9c3bce7f9b6dfdff2
wenderson gustavo <wgustavo.dev@gmail.com>
2024-05-23 19:45:17 -0300

    Header quase pronto

--- blogr-landing-page/app/page.jsx
+++ blogr-landing-page/app/page.jsx
@@ -1,7 +1,9 @@
+import Header from '@/components/header/Header';
+
 export default function Home() {
   return(
-    <div>
-      <h1>Hello World </h1>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
--- blogr-landing-page/src/components/header/Header.jsx
+++ blogr-landing-page/src/components/header/Header.jsx
@@ -1,3 +1,43 @@
-export default function Header() {
+import styles from './Header.module.css';
+import Image from 'next/image';
+import logo from '@images/logo.svg';
+import Link from 'next/link';
 
+export default function Header() {
+  return (  
+    <header className={styles.header}>
+      <div className={`${styles.headerContainer} wrapper`}>
+
+        <div className={styles.headerNavbar}>
+
+          <Link href='#'><div><Image src={logo} alt='Logo'/></div></Link>
+
+          <nav className={styles.navbar}>
+            <ul>
+              <li>Product</li>  
+              <li>Company</li>
+              <li>Connect</li>
+            </ul>
+          </nav>
+          
+          <div className={styles.headerButtonAuth}>
+            <button>Login</button>
+            <button>Sign Up</button>
+          </div>
+
+        </div>
+     
+        <div className={styles.headerText}>
+          <h1>A modern publishing platform</h1>
+          <h2>Grow your audience and build your online brand</h2>
+          <div>
+            <button>Start for Free</button>
+            <button>Learn More</button>
+          </div>
+          
+        </div>
+   
+      </div>
+    </header>
+  );
 }
